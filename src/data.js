db884959bd833cf913f1311bdc9000997ca12ea5
Paz Karina <60518586+PazKarina@users.noreply.github.com>
2020-03-13 13:33:11 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,8 +3,12 @@ import data from './data/rickandmorty/rickandmorty.js';
 //funcion para encontrar desde la data a los 5 personajes principales
 export const principalCharacters = data.results.filter(char =>
 (char.id==1||char.id==2||char.id==3||char.id==4||char.id==5))
-
+ 
+//exportar todos los personajes
 export const allCharacters = data.results
 
+//exportar todos los Ricsk´s
+export const allRicks = data.results.filter(value =>(value.name.includes ("Rick"))) 
 
-//funcion para traer de la data las caracteristicas de todos los persanejes
+//exportar todos los Morty´s
+export const allMortys = data.results.filter(value =>(value.name.includes ("Morty"))) 
