f75a0687fc7cebdf5940c65096edf2a5d59a054a
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-03-16 10:20:22 -0600

    ultimos cambios

--- src/data.js
+++ src/data.js
@@ -3,14 +3,21 @@ import data from './data/rickandmorty/rickandmorty.js';
 //funcion para encontrar desde la data a los 5 personajes principales
 export const principalCharacters = data.results.filter(char =>
 (char.id==1||char.id==2||char.id==3||char.id==4||char.id==5))
-
+console.log(principalCharacters)
 
 export const allCharacters = data.results
+console.log(allCharacters)
+// export const all =() =>{
+// allCharacters.forEach(i) =>{
+//     let idChar = allCharacters
+//     idChar.id = i
+// }
 
 
+export const ricks = data.results.filter(value => (value.name.includes("Rick")))
+console.log(ricks)
 
-
-
+export const mortys = data.results.filter(value => (value.name.includes("Morty")))
 // export const principalCharacters = data.results.filter(char => {
 
 //     if (char.id === e.target.id){
--- src/main.js
+++ src/main.js
@@ -1,48 +1,80 @@
-import {principalCharacters, allCharacters} from './data.js';
-document.querySelector(".screenCharacters").style.display="none";
-const a = principalCharacters
-function btnRick (e){
-const b = e.target.id
-    document.querySelector("#idName").innerHTML = 
-    `<li>Nombre: ${(a[b].name)}</li><br>
-    <li>Status: ${(a[b].status)}</li><br>
-    <li>Especie: ${(a[b].species)}</li><br>
-    <li>Genero: ${(a[b].gender)}</li>
-    <li>Origin: ${(a[b].origin.name)}</li>`
-};
-const btns = document.querySelectorAll('.btn')
-btns.forEach(el=>{el.addEventListener("click", btnRick)})
+import {principalCharacters, allCharacters, ricks, mortys} from './data.js';
 
 
-
-
-
-
-//habilitar Pantallas
+//AllCharacters
 function screenCharacters () {
-    let screen = document.querySelector(".principalScreen")
+    let screen = document.querySelector(".principalScreen");
     screen.style = "display:none";
-    document.querySelector(".screenCharacters").style.display="block";
+    document.querySelector("#screenCharacters").style.display="block";
 }
+document.querySelector("#screenCharacters").style.display="none";
 document.querySelector(".btnAllCharacters").addEventListener("click", screenCharacters)
 
-function screenRicks () {
-
+//Ricks
+function screenMultiRicks (){
+    let screen = document.querySelector(".principalScreen");
+    screen.style = "display:none";
+    document.querySelector("#screenRicks").style.display = "block";
 }
+document.querySelector(".btnRicks").addEventListener("click",screenMultiRicks)
+document.querySelector("#screenRicks").style.display = "none";
 
-    //mostrar sólo imagen y nombre de los personajes
+//Mortys
+function screenMultiMortys (){
+    let screen = document.querySelector(".principalScreen");
+    screen.style = "display:none";
+    document.querySelector("#screenMortys").style.display = "block";
+}
+document.querySelector(".btnMortys").addEventListener("click",screenMultiMortys)
+document.querySelector("#screenMortys").style.display = "none";
+
+//close
+function close(){
+    let close = document.querySelector("#screenCharacters");
+    close.style = "display:none"
+    document.querySelector(".principalScreen").style.display = "block";
+}
+document.querySelector("#close1").addEventListener("click", close)
+
+function close2(){
+    let close = document.querySelector("#screenRicks");
+    close.style = "display:none"
+    document.querySelector(".principalScreen").style.display = "block";
+}
+document.querySelector("#close2").addEventListener("click", close2)
+
+function close3(){
+    let close = document.querySelector("#screenMortys");
+    close.style = "display:none"
+    document.querySelector(".principalScreen").style.display = "block";
+}
+document.querySelector("#close3").addEventListener("click", close3)
+
+//Intergalactic ID
+
+function idCard (e){
+    document.querySelector("#idName").innerHTML = 
+    `<li>Nombre: ${(principalCharacters[e.target.id].name)}</li><br>
+    <li>Status: ${(principalCharacters[e.target.id].status)}</li><br>
+    <li>Especie: ${(principalCharacters[e.target.id].species)}</li><br>
+    <li>Genero: ${(principalCharacters[e.target.id].gender)}</li><br>
+    <li>Origen: ${(principalCharacters[e.target.id].origin.name)}</li>`
+};
+const btns = document.querySelectorAll('.principalButton')
+btns.forEach(el=>{el.addEventListener("click",idCard)})
+ 
+
+//mostrar todos los personajes
 const showCharacters = ()=> {
  allCharacters.forEach((item, i) => {
     let boxCharacters=item.image; 
     let nameCharacter= item.name;
-    let container= document.createElement('div')
     let name= document.createElement('p');
     let img= document.createElement('img');
     let btn= document.createElement('button');
     let node=btn;
-    container.appendChild(name);
-    container.appendChild(img);
-    btn.appendChild(container);
+    btn.appendChild(name);
+    btn.appendChild(img);
     name.innerHTML = nameCharacter;
     img.src=boxCharacters;
     name.style.fontFamily = "kindergarten";
@@ -50,15 +82,72 @@ const showCharacters = ()=> {
     name.style.color = "white";
     btn.style.border ="none";
     btn.style.background="0,0,0,0.0"
-    container.style.background = "0,0,0,0.0";
-    container.style.width= "20em";
-    container.style.height= "20em";
+    btn.style.width= "20em";
+    btn.style.height= "20em";
     img.style.width = "14em";
     img.style.height = "14em";
 
-    container.id = i
-
-    document.querySelector(".appendchild").appendChild(node);
+    allCharacters.id = i
+    btn.className= "allbtn"
+    document.querySelector("#appendchild1").appendChild(node);
  })
 }
-document.querySelector(".btnAllCharacters"). addEventListener("click", showCharacters)
+document.querySelector(".btnAllCharacters").addEventListener("click", showCharacters)
+
+// mostrar todos los Ricks
+const showRicks = ()=> {
+    ricks.forEach((item) => {
+       let boxCharacters=item.image; 
+       let nameCharacter= item.name;
+       let name= document.createElement('p');
+       let img= document.createElement('img');
+       let btn= document.createElement('button');
+       let node=btn;
+       btn.appendChild(name);
+       btn.appendChild(img);
+       name.innerHTML = nameCharacter;
+       img.src=boxCharacters;
+       name.style.fontFamily = "kindergarten";
+       name.style.fontSize = "20px";
+       name.style.color = "white";
+       btn.style.border ="none";
+       btn.style.background="0,0,0,0.0"
+       btn.style.width= "20em";
+       btn.style.height= "20em";
+       img.style.width = "14em";
+       img.style.height = "14em";
+   
+       btn.className= "allbtn"
+       document.querySelector("#appendchild2").appendChild(node);
+    })
+}
+document.querySelector(".btnRicks").addEventListener("click", showRicks)
+
+// mostrar todos los Mortys
+const showMortys = ()=> {
+    mortys.forEach((item) => {
+       let boxCharacters=item.image; 
+       let nameCharacter= item.name;
+       let name= document.createElement('p');
+       let img= document.createElement('img');
+       let btn= document.createElement('button');
+       let node=btn;
+       btn.appendChild(name);
+       btn.appendChild(img);
+       name.innerHTML = nameCharacter;
+       img.src=boxCharacters;
+       name.style.fontFamily = "kindergarten";
+       name.style.fontSize = "20px";
+       name.style.color = "white";
+       btn.style.border ="none";
+       btn.style.background="0,0,0,0.0"
+       btn.style.width= "20em";
+       btn.style.height= "20em";
+       img.style.width = "14em";
+       img.style.height = "14em";
+   
+       btn.className= "allbtn"
+       document.querySelector("#appendchild3").appendChild(node);
+    })
+}
+document.querySelector(".btnMortys").addEventListener("click", showMortys)
